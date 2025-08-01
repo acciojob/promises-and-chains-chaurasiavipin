a1cd5eb1091ae1fa416a649f336b706eab0e01b6
chaurasiavipin <vipinchaurasia481@gmail.com>
2025-08-01 11:03:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,16 +9,21 @@ submimtbtn.addEventListener('click', ()=> {
 	const input1=parseInt(document.getElementById("age").value);
 const input2=document.getElementById("name").value;						
 	let promis=new Promise((resolve,reject)=> {
-	
+	if(input1 == "" || input2== ""){
+		alert("Please enter valid details.")
+		return;
+	}
+		
 	setTimeout(()=> {
-		if(input1 !="" && input1>=18){
+		if( input1>=18){
 		resolve(input1)
 		}
 		else{
 			reject(input2)
 		}
-		
+	
 	}, 4000)	
+	
 })
  promis.then(function (userName) {
     alert(`Welcome, ${userName}. You can vote.`);
