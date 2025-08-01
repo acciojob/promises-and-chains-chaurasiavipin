9cdc37c39ea7b5f96e7d9e7b90a328d437da324f
chaurasiavipin <vipinchaurasia481@gmail.com>
2025-08-01 10:34:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+
+
+
+const submimtbtn=document.getElementById("btn")
+ 
+
+submimtbtn.addEventListener('click', ()=>{
+							
+	const input1=parseInt(document.getElementById("age").value);
+const input2=document.getElementById("name").value;						
+	let promis=new Promise((resolve,reject)=> {
+	
+	setTimeout(()=>{
+		if(input1 !="" && input1>=18){
+		resolve(input1)
+		}
+		else{
+			reject(input1)
+		}
+		
+	}, 4000)	
+})
+ promis.then(function (userName) {
+    alert(`Welcome, ${userName}. You can vote.`);
+  }).catch(function (userName) {
+    alert(`Oh sorry ${userName}. You aren't old enough.`);
+  });
+});
+})
